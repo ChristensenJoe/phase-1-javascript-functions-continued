c2c882db738c9653029c16107b4203693b3ea5fd
Joe Christensen <joechristensenwork@gmail.com>
2021-06-28 11:10:54 -0700

    commit tree 0de2656e419da83f56b34dd1586ef37acb2ad02d

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code here
+function saturdayFun(event="roller-skate") {
+    return `This Saturday, I want to ${event}!`;
+}
+
+const mondayWork = function(event="go to the office") {
+    return `This Monday, I will ${event}.`;
+}
+
+function wrapAdjective(thing="*") {
+    return function(param="special") {
+        return `You are ${thing}${param}${thing}!`;
+    }
+}
